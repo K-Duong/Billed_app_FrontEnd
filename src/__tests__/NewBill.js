ec5ec61a2c1af59298be53ebebc06ade9720a8c5
Duong TRINH <khanhduong.dav@gmail.com>
2024-02-19 22:22:58 +0100

    finish test unit NewBill.js test for function handleSubmit

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -4,14 +4,12 @@
 
 import "@testing-library/jest-dom";
 import { fireEvent, screen, waitFor } from "@testing-library/dom";
+import NewBillUI from "../views/NewBillUI.js";
 import NewBill from "../containers/NewBill.js";
-import { ROUTES, ROUTES_PATH } from "../constants/routes";
-// import { bills } from "../fixtures/bills.js";
-
+import { ROUTES_PATH } from "../constants/routes";
 import { localStorageMock } from "../__mocks__/localStorage.js";
 import mockStore from "../__mocks__/store";
 import router from "../app/Router.js";
-// import BillsUI from "../views/BillsUI.js";
 
 jest.mock("../app/Store", () => mockStore);
 
@@ -44,19 +42,19 @@ describe(" Given I am connected as an employee and I am on NewBill Page", () => 
       target: { files: [file] },
     };
 
-    const bill = new NewBill({
+    const newBill = new NewBill({
       document,
       onNavigate,
       store: mockStore,
       localStorage,
     });
 
-    const mockHandleChangeFile = jest.fn((e) => bill.handleChangeFile(e));
+    const mockHandleChangeFile = jest.fn((e) => newBill.handleChangeFile(e));
     input.addEventListener("change", mockHandleChangeFile);
     await waitFor(() => fireEvent.change(input, event));
 
     expect(mockHandleChangeFile).toHaveBeenCalled();
-    expect(bill.fileName).toBe("test.jpg");
+    expect(newBill.fileName).toBe("test.jpg");
   });
   test("Then when I upload an pdf file, input is not valid", async () => {
     const file = new File(["test file content"], "test.pdf", {
@@ -69,145 +67,46 @@ describe(" Given I am connected as an employee and I am on NewBill Page", () => 
       target: { files: [file] },
     };
 
-    const bill = new NewBill({
+    const newBill = new NewBill({
       document,
       onNavigate,
       store: mockStore,
       localStorage,
     });
 
-    const mockHandleChangeFile = jest.fn((e) => bill.handleChangeFile(e));
+    const mockHandleChangeFile = jest.fn((e) => newBill.handleChangeFile(e));
     input.addEventListener("change", mockHandleChangeFile);
     await waitFor(() => fireEvent.change(input, event));
 
     expect(mockHandleChangeFile).toHaveBeenCalled();
-    expect(bill.fileName).toBeNull();
+    expect(newBill.fileName).toBeNull();
   });
 
-  // describe("When I finish the form and click on submit button", () => {
-  //   beforeEach(() => {
-  //     Object.defineProperty(window, "localStorage", {
-  //       value: localStorageMock,
-  //     });
-
-  //     window.localStorage.setItem(
-  //       "user",
-  //       JSON.stringify({
-  //         type: "Employee",
-  //       })
-  //     );
-  //     const root = document.createElement("div");
-  //     root.setAttribute("id", "root");
-  //     document.body.append(root);
-  //     router();
-  //     window.onNavigate(ROUTES_PATH.NewBill);
-  //   })
-  //   test("function handleSubmit has been called", async () => {
-  //     // const input = screen.getByTestId("file");
-  //     const form = screen.getByTestId("form-new-bill");
-  //     // const email = JSON.parse(localStorage.getItem("user")).email;
-  //     // const file = new File(["test file content"], "test.jpg", {
-  //     //   type: "image/jpeg",
-  //     // });
-  //     // const valForm = {
-  //     //   email,
-  //     //   type: "Transports",
-  //     //   name: "Train Lyon Paris",
-  //     //   amount: "100",
-  //     //   date: "2024-01-10",
-  //     //   vat: "10",
-  //     //   pct: "10",
-  //     //   commentary: "merci",
-  //     //   fileUrl: null,
-  //     //   fileName: "test.jpg",
-  //     //   status: "pending",
-  //     // };
-  //     const onNavigate = (pathname) => {
-  //       document.body.innerHTML = ROUTES({ pathname });
-  //     };
-  //     window.onNavigate(ROUTES_PATH.NewBill);
-  //     const preventDefault = jest.fn();
-  //     const event = {
-  //       preventDefault,
-  //       target: { files: [file] },
-  //     };
-
-  //     const bill = new NewBill({
-  //       document,
-  //       onNavigate,
-  //       store: mockStore,
-  //       localStorage,
-  //     });
-  //     // await waitFor(() => {
-  //     //   fireEvent.change(screen.getByTestId("expense-type"), {
-  //     //     target: { value: valForm.type },
-  //     //   });
-  //     //   fireEvent.change(screen.getByTestId("expense-name"), {
-  //     //     target: { value: valForm.name },
-  //     //   });
-  //     //   fireEvent.change(screen.getByTestId("amount"), {
-  //     //     target: { value: valForm.amount },
-  //     //   });
-  //     //   fireEvent.change(screen.getByTestId("datepicker"), {
-  //     //     target: { value: valForm.date },
-  //     //   });
-  //     //   fireEvent.change(screen.getByTestId("vat"), {
-  //     //     target: { value: valForm.vat },
-  //     //   });
-  //     //   fireEvent.change(screen.getByTestId("pct"), {
-  //     //     target: { value: valForm.pct },
-  //     //   });
-  //     //   fireEvent.change(screen.getByTestId("commentary"), {
-  //     //     target: { value: valForm.commentary },
-  //     //   });
-  //     // });
-
-  //     const mockHandleChangeFile = jest.fn((e) => bill.handleChangeFile(e));
-  //     input.addEventListener("change", mockHandleChangeFile);
-  //     await waitFor(() => fireEvent.change(input, event));
-
-  //       screen.getByTestId("expense-type").value = valForm.type;
-  //       screen.getByTestId("expense-name").value = valForm.name;
-  //       screen.getByTestId("amount").value = valForm.amount;
-  //       screen.getByTestId("datepicker").value = valForm.date;
-  //       screen.getByTestId("vat").value = valForm.vat;
-  //       screen.getByTestId("pct").value = valForm.pct;
-  //       screen.getByTestId("commentary").value = valForm.commentary;
-
-  //     expect(screen.getByTestId("expense-type")).toBeTruthy();
-  //     expect(screen.getByTestId("expense-type").value).toBe(valForm.type);
-  //     expect(screen.getByTestId("expense-name").value).toBe(valForm.name);
-  //     expect(screen.getByTestId("amount").value).toBe(valForm.amount);
-  //     expect(screen.getByTestId("datepicker").value).toBe(valForm.date);
-  //     expect(screen.getByTestId("vat").value).toBe(valForm.vat);
-  //     expect(screen.getByTestId("pct").value).toBe(valForm.pct);
-  //     expect(screen.getByTestId("commentary").value).toBe(valForm.commentary);
-
-  //     //   expect(mockHandleChangeFile).toHaveBeenCalled();
-  //     //   expect(form).toBeTruthy();
-  //     // const mockHandleSubmit = jest.fn((e) => bill.handleSubmit(e));
-  //     // form.addEventListener("submit", mockHandleSubmit);
-  //     //   //   //   await waitFor(() => fireEvent.submit(form));
-  //     // fireEvent.submit(form);
-  //     // // document.body.innerHTML = BillsUI({data: bills});
-  //     // expect(mockHandleSubmit).toHaveBeenCalledTimes(1);
-  //   //   fireEvent.submit(form, {
-  //   //     preventDefault,
-  //   //     target: {
-  //   //         querySelector: (selector) => document.querySelector(selector)
-  //   //     }
-  //   //   });
-  //   });
-  //   // test("New bill appears on list of bills on Bill page", () => {});
-  // });
 });
+describe("When I finish the form and click on submit button", () => {
+  test("Then function handleSubmit has been called", async () => {
+    document.body.innerHTML = NewBillUI()
+    const newBill = new NewBill({
+      document,
+      onNavigate,
+      store: null,
+      localStorage: window.localStorage,
+    });
 
+    const handleSubmit = jest.fn((e) => newBill.handleSubmit(e));
+    const formNewBill = screen.getByTestId("form-new-bill");
+    formNewBill.addEventListener("submit", handleSubmit);
+    fireEvent.submit(formNewBill);
+
+    expect(handleSubmit).toHaveBeenCalled();
+  })
+})
 
 //Test d'intégration
 describe("When an error occurs on API", () => {
   beforeEach(() => {
     jest.spyOn(mockStore, "bills");
-    jest.spyOn(console, "error").mockImplementation();
+    jest.spyOn(console, "error");
     const root = document.createElement("div");
     root.setAttribute("id", "root");
     document.body.appendChild(root);
@@ -229,7 +128,7 @@ describe("When an error occurs on API", () => {
     await new Promise(process.nextTick);
     expect(console.error).toHaveBeenCalledTimes(1);
     expect(console.error.mock.calls[0][0].toString()).toBe(error404.toString());
-  });
+   });
 
   test("fetches messages from an API and fails with 500 message error", async () => {
     const error500 = new Error("Erreur 500");
